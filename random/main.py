35e1c071eb5eed6af338ca77c634b23a4084b827
rogrzhng <zhangjtroger@outlook.com>
2022-04-08 08:27:33 +0800

    我牛逼

---
+++ random/main.py
@@ -0,0 +1,21 @@
+import random
+cpu=random.randint(0,10);
+count=0
+while True:
+    if count>=5:
+        print("你寄了")
+        break
+
+    ins=input("请输入你的猜测:")
+    ins=int(ins)
+
+    count+=1
+    if ins == cpu:
+        print("你牛逼！")
+        print("你猜了{}次就猜中啦！".format(count))
+        break;
+    elif ins<cpu:
+        print("小了")
+    elif ins>cpu:
+        print("大了")
+
